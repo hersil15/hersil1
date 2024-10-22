e9ec753926cc93619904950beeb4ae0c317f954f
hersil15 <chefherasi@gmail.com>
2024-10-22 11:15:35 -0400

    actuallizando 22-10

---
+++ javascrit/seccion1.js
@@ -0,0 +1,56 @@
+//alert("me ejecuto desde un archivo externo");
+//comentario de una sola linea
+/*
+cometario de varias lineas
+
+*/
+//la consola
+console.log("hola mundo");
+
+//VARIABLES
+/* incializacion de variable*/
+var edad = 5;
+
+// ultilizacion de variable
+console.log(edad);
+
+// TIPOS DE DATOS
+//Numeros
+var numero = 5;
+var numero_float= 5.5;
+//Cadena de texto
+var cadena = "Hola mundo";
+//booleanos
+var booleanos = true
+// booleanos = false;
+//Arreglos (Arrayz)
+var arreglo =[1,2,3,4,5,];
+var frutas = ["manzana", "pera", "uva"];
+// Objetos
+var obejto ={
+  nombre: "Herasi",
+  edad: "46",
+};
+var carro = {
+  marca: "Toyota",
+  modelo: "corolla",
+  ario: "2024",
+}
+// undefined
+var indefinido;
+//Null
+var nulo= null:
+
+//OPERADORES COMPATIVOS
+//Aritmeticos
+var suma = 5+3;
+var resta = 5-3;
+var multiplicar = 5*3;
+var division = 5/3;
+
+//Modulo
+var modulo = 5 % 2;
+
+
+
+
